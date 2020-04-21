a484fe9b96bd8d44068d88543127eeead53461f1
sousajo <josesousa@rentalcars.com>
2020-04-21 11:43:13 +0100

    initial commit

---
+++ src/test/java/com/machesterdigital/event_manager/EventManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.machesterdigital.event_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
