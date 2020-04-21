e376dbb62e6ce0c11e4e02662ad725ba23db1462
Johanna Longmuir <johannalongmuir@gmail.com>
2020-04-21 12:38:30 +0100

    Add Datasource Config and Event table

--- src/main/java/com/machesterdigital/event_manager/DatasourceConfig.java
+++ src/main/java/com/machesterdigital/event_manager/DatasourceConfig.java
@@ -1,5 +1,9 @@
 package com.machesterdigital.event_manager;
 
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 import javax.sql.DataSource;
 
 @Configuration
@@ -7,8 +11,12 @@ class DatasourceConfig {
 
     @Bean
     public DataSource dataSource(){
-        return DataSourceBuilder.create
+        return DataSourceBuilder.create()
+                .driverClassName("com.mysql.jdbc.Driver")
+                .url("jdbc:mysql://db-workshop.public-dev.zuto.cloud/bootcamp1")
+                .username("bootcamp1")
+                .password("LlamaPancakes")
+                .build();
     }
-
 
 }
