99d9c1d56bc4b0b2ae493b39d462272b8db60c6c
josousa82 <j.alves.sousa82@gmail.com>
2020-04-27 16:36:33 +0100

    Added docker compose and script that cheks if external network is ready to docker, if not will createit. will be integrated with gradle.

--- src/main/resources/database/data/01-populatedata-schema.sql
+++ src/main/resources/database/data/01-populatedata-schema.sql
@@ -1,4 +1,3 @@
-INSERT INTO events(event_name, start_date, end_date)
-    VALUES ('evento 1', '10-05-2020', '11-05-2020'),
-           ('evento 2', '10-05-2020', '11-05-2020'),
-           ('evento 3', '10-05-2020', '11-05-2020');
+
+
+
