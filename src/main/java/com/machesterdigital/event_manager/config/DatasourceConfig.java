555bb8be6905fb65557429b4a1c47bd1b8f28829
Johanna Longmuir <johannalongmuir@gmail.com>
2020-04-28 13:54:39 +0100

    Refactor DataSource config. Add @Primary for Docker database

--- src/main/java/com/machesterdigital/event_manager/config/DatasourceConfig.java
+++ src/main/java/com/machesterdigital/event_manager/config/DatasourceConfig.java
@@ -3,23 +3,25 @@ package com.machesterdigital.event_manager.config;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 import javax.sql.DataSource;
 
 @Configuration
 class DatasourceConfig {
-//
-//    @Bean(name = "bootcamp")
-//    public DataSource dataSource(){
-//        return DataSourceBuilder.create()
-//                .driverClassName("com.mysql.jdbc.Driver")
-//                .url("jdbc:mysql://db-workshop.public-dev.zuto.cloud/bootcamp1")
-//                .username("bootcamp1")
-//                .password("LlamaPancakes")
-//                .build();
-//    }
+
+    @Bean(name = "bootcamp")
+    public DataSource dataSource(){
+        return DataSourceBuilder.create()
+                .driverClassName("com.mysql.jdbc.Driver")
+                .url("jdbc:mysql://db-workshop.public-dev.zuto.cloud/bootcamp1")
+                .username("bootcamp1")
+                .password("LlamaPancakes")
+                .build();
+    }
 
     @Bean(name = "local")
+    @Primary
     public DataSource dataSourceLocal(){
         return DataSourceBuilder.create()
                 .driverClassName("com.mysql.cj.jdbc.Driver")
